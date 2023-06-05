151c2272424ea38408e3317ba79ca644ce525667
Felipe Breeze <felipeaguiarfonseca@gmail.com>
2023-06-05 18:23:48 -0300

    third commit

--- testando.js
+++ testando.js
@@ -1,1 +1,24 @@
-console.log('teste111111111111111');
+// Function to calculate the factorial of a number
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// Function to check if a number is prime
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  } else if (num === 2) {
+    return true;
+  } else {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
